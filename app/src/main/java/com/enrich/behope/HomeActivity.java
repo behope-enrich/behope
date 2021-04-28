1e9adaaea16f5896de93707a3ad35ad5f86d5a90
SunnyKadivar <sunnypatel.sp212@gmail.com>
2021-04-28 13:58:46 +0530

    bottomnevigation

--- app/src/main/java/com/enrich/behope/HomeActivity.java
+++ app/src/main/java/com/enrich/behope/HomeActivity.java
@@ -1,12 +1,18 @@
 package com.enrich.behope;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.enrich.behope.ui.login.LoginFragment;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 import java.util.HashMap;
 
@@ -14,8 +20,9 @@ public class HomeActivity extends AppCompatActivity {
 
 //    TextView uname,session;
 //    Button logout;
+    TextView HomeUserName;
 
-
+    MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +42,10 @@ public class HomeActivity extends AppCompatActivity {
 //
 //        //uname.setText( "Hey \n"+ pn +" \n"+pw );
 //
-//        SessionManager sessionManager = new SessionManager( this,SessionManager.SESSION_USERSESSION );
-//        HashMap<String,String> userDetails = sessionManager.getUserDetailFromSession();
+        SessionManager sessionManager = new SessionManager( this,SessionManager.SESSION_USERSESSION );
+        HashMap<String,String> userDetails = sessionManager.getUserDetailFromSession();
 //
-//        String uname = userDetails.get( SessionManager.KEY_USERNAME );
+        String uname = userDetails.get( SessionManager.KEY_USERNAME );
 //        String upassword = userDetails.get( SessionManager.KEY_PASSWORD );
 //        String uphoneno = userDetails.get( SessionManager.KEY_PHONENUMBER );
 //        String uemail = userDetails.get( SessionManager.KEY_EMAIL );
@@ -57,9 +64,66 @@ public class HomeActivity extends AppCompatActivity {
 //        } );
 //
 
+        HomeUserName = findViewById( R.id.txtHomeUserName );
+        String name = getIntent().getStringExtra( "name" );
+
+        HomeUserName.setText( uname.toUpperCase() );
+
+        bottomNavigation = findViewById( R.id.bottom_navigation );
+
+        bottomNavigation.add( new MeowBottomNavigation.Model( 1,R.drawable.ic_baseline_home ) );
+        bottomNavigation.add( new MeowBottomNavigation.Model( 2,R.drawable.ic_baseline_person ) );
+
+        bottomNavigation.setOnShowListener( new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment=null;
+
+                switch (item.getId()){
+                    case 1:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case 2:
+                        //fragment = new ProfileFragment();
+                        fragment = new ProfileFragment();
+                        break;
 
 
+                }
+                loadFragment(fragment);
+            }
+        } );
 
+        //bottomNavigation.setCount( 1,"10" );
+
+        bottomNavigation.show( 2,true );
+
+        bottomNavigation.setOnClickMenuListener( new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText( getApplicationContext()
+//                ,"You Clicked" + item.getId()
+//                ,Toast.LENGTH_SHORT).show();
+            }
+        } );
+
+        bottomNavigation.setOnReselectListener( new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText( getApplicationContext()
+//                ,"You Reslected" + item.getId()
+//                ,Toast.LENGTH_SHORT).show();
+            }
+        } );
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace( R.id.frame_layout,fragment )
+                .commit();
     }
 
 }
